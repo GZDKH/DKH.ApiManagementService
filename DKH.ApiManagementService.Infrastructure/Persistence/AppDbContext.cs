f138bb79ea54e52c7185143c1d6161f5277301af
Denis Kharchenko <itprodavets@gmail.com>
2026-02-15 02:58:01 +0800

    feat(audit): override GetCurrentUserId in AppDbContext
    Enable automatic EF Core audit field population (CreatorId,
    LastModifierId, DeleterId) via PlatformAuditProcessor.
    
    Fixes GZDKH/DKH.ApiManagementService#9

--- DKH.ApiManagementService.Infrastructure/Persistence/AppDbContext.cs
+++ DKH.ApiManagementService.Infrastructure/Persistence/AppDbContext.cs
@@ -2,7 +2,9 @@ using System.Reflection;
 using DKH.ApiManagementService.Application.Abstractions;
 using DKH.ApiManagementService.Domain.Entities;
 using DKH.Platform.EntityFrameworkCore;
+using DKH.Platform.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
 
 namespace DKH.ApiManagementService.Infrastructure.Persistence;
 
@@ -18,5 +20,11 @@ public class AppDbContext(DbContextOptions<AppDbContext> options)
     {
         modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
         base.OnModelCreating(modelBuilder);
+    }
+
+    protected override Guid? GetCurrentUserId()
+    {
+        var currentUser = this.GetService<IPlatformCurrentUser>();
+        return currentUser?.UserId;
     }
 }
